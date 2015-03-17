6f2c7e367a954f9b185f94fc825a52b196e392ba
Geoff Kimball <gakimball@bsu.edu>
2015-03-16 23:18:00 -0700

    Add new lithographs and prune unused images

--- lithographs.php
+++ lithographs.php
@@ -7,9 +7,12 @@
 <div id="gallery">
 	<ul id="gallery-list">
 		<p>Lithographs</p>
-		<li><a href="#church-street" class="active">Church Street</a></li>
+		<li><a href="#amazing-grace" class="active">Amazing Grace</a></li>
+		<li><a href="#summer-delight">Summer Delight</a></li>
+		<li><a href="#nighttime-friends">Nighttime Friends</a></li>
+		<li><a href="#church-street">Church Street</a></li>
 		<li><a href="#color-in-life">Color in Life</a></li>
-		<!-- <li><a href="#evelytons-pineapple">Evelyton's Pineapple</a></li> -->
+		<li><a href="#evelytons-pineapple">Evelyton's Pineapple</a></li>
 		<!-- <li><a href="#give-thanks">Give Thanks in All Things</a></li> -->
 		<!-- <li><a href="#joshua">Joshua 24:15</a></li> -->
 		<li><a href="#lincoln">Lincoln and Friend</a></li>
@@ -18,6 +21,27 @@
 		<li><a href="#pear-in-cup">Pear in Cup</a></li>
 	</ul>
 	<ul id="gallery-view">
+		<li class="gallery-item" id="amazing-grace">
+			<img src="img/lithographs/church-street.jpg" alt="Amazing Grace" class="gallery-img vert" />
+			<div class="gallery-info">
+				<h3>Amazing Grace</h3>
+				<p>6"x8" • 9"x12"</p>
+			</div>
+		</li>
+		<li class="gallery-item" id="summer-delight">
+			<img src="img/lithographs/summer-delight.jpg" alt="Summer Delight" class="gallery-img vert" />
+			<div class="gallery-info">
+				<h3>Summer Delight</h3>
+				<p>8"x10"</p>
+			</div>
+		</li>
+		<li class="gallery-item" id="nighttime-friends">
+			<img src="img/lithographs/nighttime-friends.jpg" alt="Nighttime Friends" class="gallery-img horz" />
+			<div class="gallery-info">
+				<h3>Nighttime Friends</h3>
+				<p>8"x10"</p>
+			</div>
+		</li>
 		<li class="gallery-item" id="church-street">
 			<img src="img/lithographs/church-street.jpg" alt="Church Street" class="gallery-img horz" />
 			<div class="gallery-info">
@@ -32,14 +56,14 @@
 				<p>11"x14"</p>
 			</div>
 		</li>
-		<!-- <li class="gallery-item" id="evelytons-pineapple">
+		<li class="gallery-item" id="evelytons-pineapple">
 			<img src="img/lithographs/evelytons-pineapple.jpg" alt="Evelyton's Pineapple" class="gallery-img vert" />
 			<div class="gallery-info">
 				<h3>Evelyton's Pineapple</h3>
 				<p>6"x8"</p>
 			</div>
 		</li>
-		<li class="gallery-item" id="give-thanks">
+		<!-- <li class="gallery-item" id="give-thanks">
 			<img src="img/lithographs/give-thanks-in-all-things.jpg" alt="Give Thanks in All Things" class="gallery-img horz" />
 			<div class="gallery-info">
 				<h3>Give Thanks in All Things</h3>
