df457654480642f647e77e0b1ad64348106463ab
Geoff Kimball <geoff@geoffkimball.com>
2016-06-20 12:26:18 -0700

    Update show schedule for 2016

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'template/header.php'; ?>
-			
+
 			<div class="col">
 				<h2>Gicle&eacute; Prints</h2>
 				<a href="giclee.php" alt="Gicle&eacute; Prints"><img src="img/giclee/fairfield.jpg" href="giclee.php" /></a>
@@ -10,37 +10,37 @@
 				<a href="lithographs.php" alt="Lithographs"><img src="img/lithographs/church-street.jpg" alt="Gentleman with Ox" /></a>
 				<p>Our fine lithographs are mounted on board with a laminate. All prints are sold in 2"-wide hand-grained frames.</p>
 			</div>
-			
-			<h2>2015 Show Schedule</h2>
+
+			<h2>2016 Show Schedule</h2>
 
 			<div class="sch-item">
 				<h3>Traditional Artisans at Zoar Harvest Festival</h3>
 				<p class="sch-place">Zoar, OH</p>
-				<p class="sch-date">Sat. August 1, 10AM&mdash;6PM</p>
-				<p class="sch-date">Sun. August 2, 10AM&mdash;5PM</p>
+				<p class="sch-date">Sat. August 6, 10AM&mdash;5PM</p>
+				<p class="sch-date">Sun. August 7, 10AM&mdash;4PM</p>
 			</div>
 
 			<div class="sch-item">
-				<h3>72nd Waterford Homes Tour &amp; Craft Exhibit</h3>
+				<h3>The Waterford Fair</h3>
 				<p class="sch-place">Waterford, VA</p>
-				<p class="sch-date">Fri.&ndash;Sun. October 2&ndash;4, 10AM&ndash;5PM</p>
+				<p class="sch-date">Fri.&ndash;Sun. October 7&ndash;9, 10AM&ndash;5PM</p>
 			</div>
-			
+
 			<h2 id="contact">Contact</h2>
-			
+
 			<div class="contact" id="con-phone">
 				<h3>Phone</h3>
 				<p>317-747-7337</p>
 			</div>
-			
+
 			<div class="contact" id="con-email">
 				<h3>Email</h3>
 				<p>mkmillison<br />@verizon.net</p>
 			</div>
-			
+
 			<div class="contact" id="con-address">
 				<h3>Address</h3>
 				<p>13206 Isle of Man Way <br>Fishers, IN 46037</p>
 			</div>
-			
+
 <?php include 'template/footer.php'; ?>
